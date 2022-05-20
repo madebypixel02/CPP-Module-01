5f11fb22330e1dd8cef09f10c7af85c5a298cddb
madebypixel02 <madebypixel02@gmail.com>
2022-05-20 16:07:12 +0200

    Added Makefile, misc improvements

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 12:26:38 by aperez-b          #+#    #+#             */
-/*   Updated: 2022/05/20 15:24:38 by aperez-b         ###   ########.fr       */
+/*   Updated: 2022/05/20 16:05:48 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 int main()
 {
-	/*{
+	{
 		Weapon club = Weapon("crude spiked club");
 		HumanA bob("Bob", club);
 		bob.attack();
 		club.setType("some other type of club");
 		bob.attack();
-	}*/
+	}
 	{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
