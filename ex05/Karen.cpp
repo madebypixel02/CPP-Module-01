eb5e72a76a43daedec6eaeccda9757519e4322f3
madebypixel02 <madebypixel02@gmail.com>
2022-05-21 20:19:00 +0200

    Added  implementation of pointer to member function

--- ex05/Karen.cpp
+++ ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: aperez-b <aperez-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 13:56:03 by aperez-b          #+#    #+#             */
-/*   Updated: 2022/05/21 14:29:21 by aperez-b         ###   ########.fr       */
+/*   Updated: 2022/05/21 20:16:17 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,43 +24,46 @@ Karen::~Karen(void)
 
 void	Karen::debug(void)
 {
-	std::cout << MAGENTA << "[DEBUG]" << DEFAULT;
-	std::cout << " I love to get extra bacon for my ";
+	std::cout << MAGENTA << "[DEBUG]   " << DEFAULT;
+	std::cout << "I love to get extra bacon for my ";
 	std::cout << "7XL-double-cheese-triple-pickle-special-ketchup burger.";
 	std::cout << " I just love it!" << std::endl;
 }
 
 void	Karen::info(void)
 {
-	std::cout << GRAY << "[INFO]" << DEFAULT;
-	std::cout << " I cannot believe adding extra bacon cost more money.";
+	std::cout << GRAY << "[INFO]    " << DEFAULT;
+	std::cout << "I cannot believe adding extra bacon cost more money.";
 	std::cout << " You don’t put enough! If you did I would not have to ask";
-	std::cout << "for it!" << std::endl;
+	std::cout << " for it!" << std::endl;
 }
 
 void	Karen::warning(void)
 {
-	std::cout << YELLOW << "[WARNING]" << DEFAULT;
-	std::cout << " I think I deserve to have some extra bacon for free.";
+	std::cout << YELLOW << "[WARNING] " << DEFAULT;
+	std::cout << "I think I deserve to have some extra bacon for free.";
 	std::cout << " I’ve been coming here for years and you just started ";
 	std::cout << "working here last month." << std::endl;
 }
 
 void	Karen::error(void)
 {
-	std::cout << RED << "[ERROR]" << DEFAULT;
-	std::cout << " This is unacceptable, I want to speak to the manager now.";
+	std::cout << RED << "[ERROR]   " << DEFAULT;
+	std::cout << "This is unacceptable, I want to speak to the manager now.";
 	std::cout << std::endl;
 }
 
 void	Karen::complain(std::string level)
 {
-	if (level == "DEBUG")
-		this->debug();
-	if (level == "INFO")
-		this->info();
-	if (level == "WARNING")
-		this->warning();
-	if (level == "ERROR")
-		this->error();
+	void		(Karen::*ptr_complain[4])(void) = {&Karen::debug, &Karen::info, &Karen::warning, &Karen::error};
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*ptr_complain[i])();
+			break ;
+		}
+	}
 }
